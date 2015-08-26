06d36c403934d5cecb3130388f066f979606b640
Christopher Moore <life.dispersing@gmail.com>
2015-08-26 17:38:38 -0400

    Match local QP surfaces
    First example failed to paste because of a static parameter left over
    from older code

--- R/QPGlobal.R
+++ R/QPGlobal.R
@@ -19,43 +19,51 @@ QPGlobal <- function(local.surfaces , unstable.eq.x , unstable.eq.y , x.bound , 
 	n.unstable.eq.x <- length(unstable.eq.x)
 	n.unstable.eq.y <- length(unstable.eq.y)
 		if(n.unstable.eq.x != n.unstable.eq.y){stop("Unstable x and y points not equal")}
+	n.unstable.pts <- length(unstable.eq.x)
 	mesh.xy <- dim(local.surfaces[[1]])
 	x.range <- max(x.bound)-min(x.bound)
 	y.range <- max(y.bound)-min(y.bound)
 
 	unstable.xy <- cbind(unstable.eq.x , unstable.eq.y) #unstable eq. as one object
-	unstable.phi.loc <- matrix(data = NA , nrow = n.unstable.eq.x , ncol = 2 , byrow = T) #local indeces
-	unstable.phi <- matrix(data = NA , nrow = n.unstable.eq.x , ncol = n.surfaces , byrow = F) #local phi values for each unstable equilibrium pair
-for (i in 1:n.unstable.eq.x){
-	x.loc <- round((unstable.xy[i,][1]-min(x.bound))/x.range*mesh.xy[1])
-	y.loc <- round((unstable.xy[i,][2]-min(y.bound))/y.range*mesh.xy[2])
-	unstable.phi.loc[i,] <- c(x.loc, y.loc)
-	}
+	unstable.phi.loc <- matrix(data = NA , nrow = n.unstable.pts , ncol = 2 , byrow = T, dimnames=list(paste("unstab.eq",1:n.unstable.pts,sep="") , c("x","y"))) #local indeces
 
-	for(i in 1: n.unstable.eq.x){
-		for (j in 1:n.surfaces)
-		unstable.phi[i,j] <- local.surfaces[[j]][unstable.phi.loc[i,1],unstable.phi.loc[i,2]]
-	}
-
-	max.phi <- which(unstable.phi == max(unstable.phi,na.rm= T),arr.ind=T)
-
-	if(nrow(max.phi) != n.unstable.eq.x){ #if not all max(phi) are the same, then they need to be aligned
-		global.max <- unstable.phi[max.phi[1,1],max.phi[1,2]]
-		phi.diff <- matrix(data = NA , nrow = n.surfaces , ncol = 1)
-		for (i in 1:n.surfaces){
-			phi.diff[i,] <- global.max - unstable.phi[max.phi[1,1],i]
+	for (i in 1:n.unstable.pts){ #indexes unstable cooridnates
+		x.loc <- round((unstable.xy[i,][1]-min(x.bound))/x.range*mesh.xy[1])
+		y.loc <- round((unstable.xy[i,][2]-min(y.bound))/y.range*mesh.xy[2])
+		unstable.phi.loc[i,] <- c(x.loc, y.loc)
 		}
-	} else {phi.diff <- matrix(data = 0 , nrow = n.surfaces , ncol = 1)}
+
+local.surfaces[[1]][940,901]
+local.surfaces[[2]][940,901]
+	unstable.phi <- matrix(data = NA , nrow = n.unstable.pts , ncol = n.surfaces , byrow = F , dimnames=list(paste("unstab.eq",1:n.unstable.pts,sep=""),paste("surface",1:n.unstable.pts,sep=""))) #local phi values for each unstable equilibrium pair
+	for(i in 1:n.unstable.pts) {
+		for (j in 1:n.surfaces) {
+		unstable.phi[i,j] <- local.surfaces[[j]][unstable.phi.loc[i,1],unstable.phi.loc[i,2]]
+		}
+	}
+
+	max.phi <- max(unstable.phi,na.rm=T)
+	max.phi.arr <- which(unstable.phi == max.phi , arr.ind=T)
+
+	if(nrow(max.phi.arr) != n.unstable.pts){ #if not all max(phi) are the same, then they need to be aligned
+		global.max <- unstable.phi[max.phi.arr[1,1],max.phi.arr[1,2]]
+		phi.diff <- matrix(data = NA , nrow = n.surfaces , ncol = 1, dimnames=list(paste("surface",1:n.surfaces,sep="")))
+		for (i in 1:n.surfaces){
+			phi.diff[i,] <- global.max - unstable.phi[max.phi.arr[1,1],i]
+		}
+	} else {
+		phi.diff <- matrix(data = 0 , nrow = n.surfaces , ncol = 1)
+	}
 
 	eq.arr.unadj <- array(data = unlist(local.surfaces) , dim = c(mesh.xy[1] , mesh.xy[2] , n.surfaces))
 	eq.arr <- array(data = NA , dim = c(mesh.xy[1] , mesh.xy[2] , n.surfaces))
 	for(i in 1:n.surfaces){
-		eq.arr[,,i] <- eq.arr.unadj[,,i]+phi.diff[1]
+		eq.arr[,,i] <- eq.arr.unadj[,,i]+phi.diff[i]
 	}
 	rm(eq.arr.unadj)
 	gc()
-	global.qp <- apply(eq.arr , c(1,2) , min)
+	global.qp <- apply(eq.arr , c(1:n.surfaces) , min)
 	rm(eq.arr)
 	gc()
 	global.qp
-}
+	}
