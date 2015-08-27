db38fce7be5dc25ca9f612fa65414c128a002314
stieha <bmarkslash7@hotmail.com>
2015-08-27 15:48:02 -0400

    cleaning up VDV example

--- R/VecDecompVec.R
+++ R/VecDecompVec.R
@@ -13,19 +13,8 @@
 #' #Example 1 from article
 #' equationx = "1.5*x*(1.0-(x/45.0))-(y*x*5.0)/(18.0+x)"
 #' equationy = "-4.0*y+((10.0*x*y)/(18.0+x))"
-#' 	VDV <- VecDecompVec(x.num.steps=1000, y.num.steps=1000, x.rhs=equationx, y.rhs=equationy, x.bound=c(-0.5,20), y.bound=c(-0.5,20))
-
-
-# x.donmain.boundaries <- c(-10,10)
-# y.donmain.boundaries <- c(-10,10)
-# length.xy <- c(15,15)
-# mesh.xy <- c(20,20)
-# x <- seq(x.limits[1], x.limits[2], length.out = length.xy[1]) + 0.5
-# y <- seq(y.limits[1], y.limits[2], length.out = length.xy[2]) + 0.5
-# eqns <- list("(y^2)/(x)" , "(x^2)/(y)")
-# f <- function(x, y) { x <- (y^2)/(x) ; y <- (x^2)/(y) }
-# z <- outer(x, y, f)
-# VecDecompVec(z)
+#' VDV <- VecDecompVec(x.num.steps=1000, y.num.steps=1000, x.rhs=equationx, y.rhs=equationy, x.bound=c(-0.5,20), y.bound=c(-0.5,20))
+#' VecDecompPlot(field=list(VDV[,,1],VDV[,,2]), dens=c(50,50), x.bound=c(-0.5,20), y.bound=c(-0.5,20))
 
 VecDecompVec <- function(x.num.steps,y.num.steps,x.rhs,y.rhs,x.bound,y.bound){
 	equations <- list(x.rhs,y.rhs)
