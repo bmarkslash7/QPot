d0cc52b1c631949024d6bf722597f2f63cd6c2ec
Christopher Moore <life.dispersing@gmail.com>
2015-09-04 14:36:17 -0400

    Ex2_test now reflects paper

--- tests/Ex2_test.R
+++ tests/Ex2_test.R
@@ -3,7 +3,6 @@
 	rm(list=ls())
 	your.favourite.number <- 6174
 	set.seed(your.favourite.number)
-	def.par <- par() # for deefault par settings for resetting plottting functions
 
 	#load libraries
 	library(QPot)
@@ -26,8 +25,8 @@
 
 	# 0.2.3 time series plots
 		TSPlot(ts.out.ex2, deltat=model.deltat)
-		par(def.par)
 		TSPlot(ts.out.ex2, deltat=model.deltat, dim=2)
+		quartz()
 		TSDensity(ts.out.ex2, dim=1)
 		TSDensity(ts.out.ex2, dim=2)
 
@@ -37,8 +36,8 @@
 		testequationy = "(x-4) + 0.2*(y-5)*(1 - (x-4)^2 - (y-5)^2)"
 		xbounds = c(-0.5, 7.5)
 		ybounds = c(-0.5, 7.5)
-		xstepnumber = 2000
-		ystepnumber = 2000
+		xstepnumber = 4000
+		ystepnumber = 4000
 		xinit = 4.15611
 		yinit = 5.987774
 
@@ -52,18 +51,18 @@
 
 
 ##### 0.5 quasi-potential vizualization!!! #####
-	QPContour(e2.global,c(1000,1000),c(-0.5,20),c(-0.5,20),c.parm=5)
+	QPContour(e2.global,c(1000,1000), x.bound=xbounds, y.bound=ybounds,c.parm=10, xlab=expression(italic(X)), ylab = expression(italic(Y)))
 
 
 ##### 0.6 vector field decompisition!!! #####
 	# 0.6.1 vector field
 	VDV <- VecDecompVec(x.num.steps=xstepnumber, y.num.steps=ystepnumber, x.rhs=testequationx, y.rhs=testequationy, x.bound=xbounds, y.bound=ybounds)
-		VecDecompPlot(field=list(VDV[,,1],VDV[,,2]), dens=c(25,25), x.bound=xbounds, y.bound=ybounds , tail.length = 0.5 , length = 0.03 , arrow.type="proportional", x.lim=c(2,6), y.lim=c(3,7))
+		VecDecompPlot(field=list(VDV[,,1],VDV[,,2]), dens=c(25,25), x.bound=xbounds, y.bound=ybounds , tail.length = 0.5 , length = 0.03 , arrow.type="proportional", x.lim=c(2,6), y.lim=c(3,7), xlab=expression(italic(X)), ylab = expression(italic(Y)))
 
 	# 0.6.2 gradient field	
 	VDG <- VecDecompGrad(e2.global)
-		VecDecompPlot(field=list(VDG[,,1],VDG[,,2]), dens=c(25,25), x.bound=xbounds, y.bound= ybounds , tail.length = 0.1 , length = 0.03 , arrow.type = "proportional", x.lim=c(2,6), y.lim=c(3,7))
+		VecDecompPlot(field=list(VDG[,,1],VDG[,,2]), dens=c(25,25), x.bound=xbounds, y.bound= ybounds , tail.length = 0.1 , length = 0.03 , arrow.type = "proportional", x.lim=c(2,6), y.lim=c(3,7), xlab=expression(italic(X)), ylab = expression(italic(Y)))
 
 	# 0.6.3 remainder field
 	VDR <- VecDecompRem(surface=e2.global, x.rhs=testequationx, y.rhs=testequationy, x.bound=xbounds, y.bound=ybounds)
-		VecDecompPlot(field=list(VDR[,,1],VDR[,,2]), dens=c(25,25), x.bound=xbounds, y.bound=xbounds , tail.length = 0.2 , length = 0.03 , arrow.type = "proportional", x.lim=c(2,6), y.lim=c(3,7))
+		VecDecompPlot(field=list(VDR[,,1],VDR[,,2]), dens=c(25,25), x.bound=xbounds, y.bound=xbounds , tail.length = 0.2 , length = 0.03 , arrow.type = "proportional", x.lim=c(2,6), y.lim=c(3,7), xlab=expression(italic(X)), ylab = expression(italic(Y)))
