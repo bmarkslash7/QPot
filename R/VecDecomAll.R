12da65d3b0a458542ab45decb0afb7cdb5bcdbda
Christopher Moore <life.dispersing@gmail.com>
2015-09-21 18:19:09 -0400

    Misc. example fixes

--- R/VecDecomAll.R
+++ R/VecDecomAll.R
@@ -8,7 +8,7 @@
 #' @param y.bound two-element vector with respective minimum and maximum y values.
 #' 
 #' @examples
-#' # First, System of equations
+#' # First, the system of equations
 #' 	equationx <- "1.54*x*(1.0-(x/10.14)) - (y*x*x)/(1.0+x*x)"
 #' 	equationy <- "((0.476*x*x*y)/(1+x*x)) - 0.112590*y*y"
 #' 
@@ -31,14 +31,11 @@
 #' # Fifth, determine global quasi-potential 
 #' 	unst.x <- c(0, 4.2008)
 #' 	unst.y <- c(0, 4.0039)
-#' 	ex1.global <- QPGlobal(local.surfaces = list(storage.eq1, storage.eq2), unstable.eq.x = unst.x, unstable.eq.y = unst.y, x.bound = bounds.x, y.bound = bounds.y)
+#' 	ex1.global <- QPGlobal(local.surfaces = list(storage.eq1, storage.eq2), unstable.eq.x = unst.x, unstable.eq.y = unst.y, x.bound = xbounds, y.bound = ybounds)
 #' 
-#' # Sixth, contour of the quasi-potential
-#' 	QPContour(ex1.global, dens = c(500,500), x.bound = xbounds, y.bound = ybounds, c.parm = 5)
-#'
-#' # Seventh, decompose the global quasi-potential into the deterministic skeleton, gradient, and remainder vector fields
-#' VDAll <- VecDecompAll(surface=e1.global, x.rhs=equationx, y.rhs=equationy, 
-#'  x.bound = xbounds, y.bound = ybounds)
+#' # Sixth, decompose the global quasi-potential into the deterministic skeleton, gradient, and remainder vector fields
+#' 	VDAll <- VecDecompAll(surface = ex1.global, x.rhs = equationx, y.rhs = equationy, 
+#' 	x.bound = xbounds, y.bound = ybounds)
 
 VecDecomAll <- function(surface,x.rhs,y.rhs,x.bound,y.bound){
 	qpr <- nrow(surface)
