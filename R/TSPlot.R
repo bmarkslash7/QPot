7ea8b871ee39fe72a76a91c6c3aeb567ae13dc59
Christopher Moore <life.dispersing@gmail.com>
2016-07-29 08:19:25 -0400

    Small plot changes

--- R/TSPlot.R
+++ R/TSPlot.R
@@ -54,10 +54,12 @@ TSPlot <- function(mat, deltat, dim = 1, xlim = NULL, ylim = NULL, xaxt.1D = "ti
 					warning("Simulation -> Inf. Try: (i) set exact y-axis limits using the ylim argument and (ii) dens = FALSE")
 				if (missing(ylim)) {
 					ylim = c(global.min, global.max)
-						if (dens == TRUE) {par(fig=c(0,0.775,0,1), new=FALSE , oma = rep(1,4))}
+						if (dens == TRUE) {par(fig=c(0,0.7,0,1), new=FALSE , oma = c(4, 4, 1, 2), mar = rep(0,4))}
 						# ifelse(xaxt.1D == "step" , x.label <- "Step" , x.label <- "Time")
 						x.label <- if (xaxt.1D == "step") "Step" else "Time"
-					plot(mat[,1], type = "l" , ylim = ylim , las = 1 , xlab = x.label , ylab = "State variables" , col = rgb(255, 0, 0, line.alpha, NULL, 255) , lwd = lwd , xaxt = "n", ...)
+					plot(mat[,1], type = "l" , ylim = ylim , las = 1 , ylab = "", xlab = "" , col = rgb(255, 0, 0, line.alpha, NULL, 255) , lwd = lwd , xaxt = "n", ...)
+					mtext(text = x.label, line = 2.5, side = 1)
+					mtext(text = "State variables", line = 2.5, side = 2)
 					lines(mat[,1] , mat[,3] , col = rgb(0,0,255,line.alpha,NULL,255) , lwd = lwd)
 					if (xaxt.1D == "step") {axis(1)
 						} else {
@@ -65,20 +67,23 @@ TSPlot <- function(mat, deltat, dim = 1, xlim = NULL, ylim = NULL, xaxt.1D = "ti
 							plot(0, type = "n" , xlim = c(0,(nrow(mat)*deltat)) , ylim = ylim , ylab = "" , xlab ="" , xaxt = "n" , yaxt = "n", ...)
 							axis(1)
 						}
-						if(dens == TRUE) {par(fig=c(0.65,1,0,1),new=TRUE)
+						if(dens == TRUE) {par(fig=c(0.675,1,0,1),new=TRUE)
 						densA <- density(mat[,2] , na.rm = T)
 						densB <- density(mat[,3] , na.rm = T)
 						y.lims <- ylim
 						x.lims <- c(min(c(densA$y, densB$y)) , max(c(densA$y, densB$y)))
-						plot(0 , type = "n" , xlab = "Density" , ylab = "" , las = 1 , xlim = x.lims , ylim = y.lims , yaxt = "n")
+						plot(0 , type = "n" , xlab = "" , ylab = "" , las = 1 , xlim = x.lims , ylim = y.lims , yaxt = "n")
+						mtext(text = "Density", side = 1, line = 2.5)
 						polygon(densA$y , densA$x, col = rgb(255,0,0,75,NULL,255) , border = rgb(255,0,0,130,NULL,255))
 						polygon(densB$y , densB$x, col = rgb(0,0,255,75,NULL,255) , border = rgb(0,0,255,130,NULL,255))
 						axis(4 , las = 1)}
 				} else {
-						if(dens == TRUE) {par(fig=c(0,0.775,0,1), new=FALSE , oma = rep(1,4))}
+						if(dens == TRUE) {par(fig=c(0,0.7,0,1), new=FALSE , oma = c(4, 4, 1, 2), mar = rep(0,4))}
 						# ifelse(xaxt.1D == "step" , x.label <- "Step" , x.label <- "Time")
 						x.label <- if (xaxt.1D == "step") "Step" else "Time"
-					plot(mat[,1], type = "l", las = 1 , ylim = ylim, xlab = x.label , ylab = "State variables" , col = rgb(255,0,0,line.alpha,NULL,255) , lwd = lwd , xaxt = "n", ...)
+					plot(mat[,1], type = "l", las = 1 , ylim = ylim, xlab ="" , ylab = "" , col = rgb(255,0,0,line.alpha,NULL,255) , lwd = lwd , xaxt = "n", ...)
+					mtext(text = x.label, line = 2.5, side = 1)
+					mtext(text = "State variables", line = 2.5, side = 2)
 					lines(mat[,1] , mat[,3] , col = rgb(0,0,255,line.alpha,NULL,255) , lwd = lwd)
 						if (xaxt.1D == "step") {axis(1)
 						} else {
@@ -86,21 +91,24 @@ TSPlot <- function(mat, deltat, dim = 1, xlim = NULL, ylim = NULL, xaxt.1D = "ti
 							plot(0, type = "n" , xlim = c(0,(nrow(mat)*deltat)), ylim = ylim , ylab = "" , xlab ="" , xaxt = "n" , yaxt = "n", ...)
 							axis(1)
 						}
-						if(dens == T) {par(fig=c(0.65,1,0,1),new=TRUE)
+						if(dens == T) {par(fig=c(0.725,1,0,1),new=TRUE)
 						densA <- density(mat[,2] , na.rm = T)
 						densB <- density(mat[,3] , na.rm = T)
 						x.lims <- c(min(c(densA$y, densB$y)) , max(c(densA$y, densB$y)))
-						plot(0 , type = "n" , xlab = "Density" , ylab = "" , las = 1 , xlim = x.lims , yaxt = "n", ylim = ylim)
+						plot(0 , type = "n" , xlab = "" , ylab = "" , las = 1 , xlim = x.lims , yaxt = "n", ylim = ylim)
+						mtext(text = "Density", side = 1, line = 2.5)
 						polygon(densA$y , densA$x, col = rgb(255,0,0,75,NULL,255) , border = rgb(255,0,0,130,NULL,255))
 						polygon(densB$y , densB$x, col = rgb(0,0,255,75,NULL,255) , border = rgb(0,0,255,130,NULL,255))
 						axis(4 , las = 1)}
 				}
 			} else { # No Inf values in timeseries
 					if (missing(ylim)) {ylim <- c(global.min, global.max)}
-						if(dens == TRUE) {par(fig=c(0,0.775,0,1), new = FALSE , oma = rep(1,4))}
+						if(dens == TRUE) {par(fig=c(0,0.7,0,1), new = FALSE , oma = c(4, 4, 1, 2), mar = rep(0,4))}
 						# ifelse(xaxt.1D == "step" , x.label <- "Step" , x.label <- "Time")
 						x.label <- if (xaxt.1D == "step") "Step" else "Time"
-					plot(mat[,1], type = "n", ylim = ylim , las = 1 , ylab = "State variables" , xlab = x.label , lwd = lwd , xaxt = "n", ...)
+					plot(mat[,1], type = "n", ylim = ylim , las = 1 , ylab = "" , xlab = , lwd = lwd , xaxt = "n", ...)
+					mtext(text = x.label, line = 2.5, side = 1)
+					mtext(text = "State variables", line = 2.5, side = 2)
 					lines(mat[,1] , mat[,2] , col = rgb(255,0,0,line.alpha,NULL,255) , lwd = lwd)
 					lines(mat[,1] , mat[,3] , col = rgb(0,0,255,line.alpha,NULL,255) , lwd = lwd)
 					if (xaxt.1D == "step") {axis(1)
@@ -109,11 +117,12 @@ TSPlot <- function(mat, deltat, dim = 1, xlim = NULL, ylim = NULL, xaxt.1D = "ti
 							plot(0, type = "n" , xlim = c(0,(nrow(mat)*deltat))  , ylab = "" , xlab ="" , xaxt = "n" , yaxt = "n", ...)
 							axis(1)
 						}
-						if(dens == TRUE) {par(fig=c(0.65,1,0,1),new=TRUE)
+						if(dens == TRUE) {par(fig=c(0.725,1,0,1),new=TRUE)
 						densA <- density(mat[,2] , na.rm = T)
 						densB <- density(mat[,3] , na.rm = T)
 						x.lims <- c(min(c(densA$y, densB$y)) , max(c(densA$y, densB$y)))
-						plot(0 , type = "n" , xlab = "Density" , ylab = "" , las = 1 , xlim = x.lims , ylim = ylim , yaxt = "n")
+						plot(0 , type = "n" , xlab = "" , ylab = "" , las = 1 , xlim = x.lims , ylim = ylim , yaxt = "n")
+						mtext(text = "Density", side = 1, line = 2.5)
 						polygon(densA$y , densA$x, col = rgb(255,0,0,75,NULL,255) , border = rgb(255,0,0,130,NULL,255))
 						polygon(densB$y , densB$x, col = rgb(0,0,255,75,NULL,255) , border = rgb(0,0,255,130,NULL,255))
 						axis(4 , las = 1)}
